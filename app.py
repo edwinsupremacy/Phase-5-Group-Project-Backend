0c24c53bb0ea30e0f2797066d0cefdee453963ab
s-k3nyyy <collo.c.heg@gmail.com>
2024-08-19 09:48:37 +0300

    collo

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 mail = Mail(app)
 bcrypt = Bcrypt(app)
 
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+CORS(app, resources={r"/*": {"origins": "https://edwinsupremacy.github.io"}})
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auction.db'
